67ad57659456fd1634ad556bd7e9743e5d3c16ca
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2021-06-20 22:55:46 +0530

    cholo cholo noyone

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,60 +1,10 @@
 import React, { useState } from 'react';
 import { Avatar, Badge, Button, Grid, IconButton, InputAdornment, TextField, Typography, useMediaQuery } from '@material-ui/core';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-// import jgec from "../../img/landingpageBackground.jpeg";
+import { useTheme } from '@material-ui/core/styles';
 import EditIcon from '@material-ui/icons/Edit';
-// import FaceIcon from "@material-ui/icons/Face";
+import useStyles from '../../styles/Profile/profile';
 
-const useStyles = makeStyles((theme) => ({
-    container: {
-        backgroundBlendMode: 'screen',
-        background: `rgba(196,196,196,0.7)`,
-        width: '100vw',
-        height: '100%',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        paddingTop: '6em',
-        paddingLeft: '1em',
-        paddingRight: '1em',
-        paddingBottom: '1em'
-    },
-    inputItem: {
-        width: '100%',
-        marginBottom: '1em'
-    },
-    inputContainer: {
-        padding: '1em'
-    },
-    notificationItem: {
-        backgroundColor: '#fff',
-        width: '100%',
-        padding: '0.7em',
-        marginBottom: '1px'
-    },
-    notificationsText: {
-        color: '#111',
-        fontFamily: 'Ubuntu, sans-serif',
-        maxWidth: '15em',
-        wordWrap: 'break-word',
-        fontSize: '1em'
-    },
-    link: {
-        fontFamily: 'Ubuntu, sans-serif',
-        fontSize: '0.8em',
-        marginLeft: 'auto',
-        color: 'blue',
-        cursor: 'pointer'
-    },
-    saveButton: {
-        backgroundColor: theme.palette.common.orange,
-        '&:hover': {
-            backgroundColor: theme.palette.common.orange
-        },
-        padding: '0.5em 2em',
-        color: '#fff'
-    }
-}));
+
 
 const Profile = () => {
     const classes = useStyles();
